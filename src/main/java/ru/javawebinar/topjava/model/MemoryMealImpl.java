cf9ee9a1136487b7e89f07b48afa3e6f5925bce7
DenisKotin <kot1n@mail.ru>
2020-06-08 23:05:31 +0300

    HW01+

--- src/main/java/ru/javawebinar/topjava/model/MemoryMealImpl.java
+++ src/main/java/ru/javawebinar/topjava/model/MemoryMealImpl.java
@@ -2,22 +2,21 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMealImpl implements MealModel {
 
-    private static List<Meal> meals = Arrays.asList(
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
-    );
+    private static Map<Integer, Meal> meals = new HashMap<Integer, Meal>(){
+        {
+          put( 1, new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+          put( 2, new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+          put( 3,new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+          put( 4,new Meal(LocalDateTime.of(2020, Month.JANUARY, 31,0,0), "Еда на граничное значение",100));
+          put( 5,new Meal(LocalDateTime.of(2020, Month.JANUARY, 31,10,0), "Завтрак",1000));
+          put( 6,new Meal(LocalDateTime.of(2020, Month.JANUARY, 31,13,0), "Обед",500));
+          put( 7,new Meal(LocalDateTime.of(2020, Month.JANUARY, 31,20,0), "Ужин",410));
+        }
+    };
 
 
     private static MemoryMealImpl instance = new MemoryMealImpl();
@@ -40,6 +39,6 @@ public class MemoryMealImpl implements MealModel {
 
     @Override
     public Optional<List<Meal>> getAll() {
-        return Optional.of(new ArrayList<>(meals));
+        return Optional.of(new ArrayList<>(meals.values()));
     }
 }
