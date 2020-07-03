89fc12e469dc5557cbbdb977b142fe167ebfd903
KDV79 <kot1n@mail.ru>
2020-07-03 07:49:01 +0300

    5 5 profiles connection pool

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -11,18 +11,5 @@ public class Profiles {
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    //  Get DB profile depending of DB driver in classpath
-    public static String getActiveDbProfile() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            return POSTGRES_DB;
-        } catch (ClassNotFoundException ex) {
-            try {
-                Class.forName("org.hsqldb.jdbcDriver");
-                return Profiles.HSQL_DB;
-            } catch (ClassNotFoundException e) {
-                throw new IllegalStateException("Could not find DB driver");
-            }
-        }
-    }
+    public static final String ACTIVE_DB = POSTGRES_DB;
 }
